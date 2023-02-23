04d3cbe6ea07dfb5efa141d81678da2726fb68f1
min <minaphamtan@gmail.com>
2023-02-23 08:56:04 +0700

    New repository

---
+++ SSR.WebAdmin/src/models/knowledgeModel.js
@@ -0,0 +1,40 @@
+﻿const toJson = (item) => {
+    return {
+        id : item.id,
+        name: item.name,
+        content : item.content ,
+        //loaiDanhMuc:item.loaiDanhMuc,
+    }
+}
+const fromJson = (item) => {
+    return {
+        id : item.id,
+        name: item.name,
+        content : item.content ,
+        //loaiDanhMuc:item.loaiDanhMuc,
+    }
+}
+
+const baseJson = () => {
+    return {
+        id : null,
+        name: null,
+        content : null ,
+        //loaiDanhMuc:null,
+    }
+}
+
+const toListModel = (items) =>{
+    if(items.length > 0){
+        let data = [];
+        items.map((value, index) =>{
+            data.push(fromJson(value));
+        })
+        return data??[];
+    }
+    return [];
+}
+
+export const knowledgeModel = {
+    toJson, fromJson, baseJson, toListModel
+}
